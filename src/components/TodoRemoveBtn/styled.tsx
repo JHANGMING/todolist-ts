2e12ca86353f4b1f4d7e5455512051b68b25cd51
jelly771001 <jelly771001@gmail.com>
2023-12-19 10:54:23 +0800

    feat: add TodoRemoveBtn

---
+++ src/components/TodoRemoveBtn/styled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper=styled.div`
+  text-align: center;
+  margin: 24px 0 36px 0;
+`;
+
+export const Button=styled.button`
+  background-color:rgba(70,34,9,.8);
+  border-radius: 8px;
+  padding: 6px 12px;
+  color: white;
+  cursor: pointer;
+  &:hover{
+    transform: scale(1.1);
+  }
+`;
